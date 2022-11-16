b531e710708b2ac4d38b0e23dffeced70c3caa5e
SkeletonSmokingWeed <118356218+SkeletonSmokingWeed@users.noreply.github.com>
2022-11-16 11:44:26 +0000

    Add files via upload

---
+++ That funny thing you know from the other day at the pot luck/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace That_funny_thing_you_know_from_the_other_day_at_the_pot_luck
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_integer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Btn_strng = new System.Windows.Forms.Button();
+            this.lblchange = new System.Windows.Forms.Label();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.lblfirst = new System.Windows.Forms.Label();
+            this.lbllast = new System.Windows.Forms.Label();
+            this.lblage = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btndisplay = new System.Windows.Forms.Button();
+            this.lblname = new System.Windows.Forms.Label();
+            this.btnfloat = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.lbladd = new System.Windows.Forms.Label();
+            this.tbnum1 = new System.Windows.Forms.TextBox();
+            this.tbnum2 = new System.Windows.Forms.TextBox();
+            this.minusbtn = new System.Windows.Forms.Button();
+            this.tbnum3 = new System.Windows.Forms.TextBox();
+            this.tbnum4 = new System.Windows.Forms.TextBox();
+            this.lblsub = new System.Windows.Forms.Label();
+            this.tbnum5 = new System.Windows.Forms.TextBox();
+            this.tbnum6 = new System.Windows.Forms.TextBox();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.lbldiv = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_integer
+            // 
+            this.Btn_integer.BackColor = System.Drawing.Color.PeachPuff;
+            this.Btn_integer.Location = new System.Drawing.Point(189, 69);
+            this.Btn_integer.Name = "Btn_integer";
+            this.Btn_integer.Size = new System.Drawing.Size(155, 38);
+            this.Btn_integer.TabIndex = 0;
+            this.Btn_integer.Text = "Integer";
+            this.Btn_integer.UseVisualStyleBackColor = false;
+            this.Btn_integer.Click += new System.EventHandler(this.Btn_integer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Btn_strng
+            // 
+            this.Btn_strng.BackColor = System.Drawing.Color.PeachPuff;
+            this.Btn_strng.Location = new System.Drawing.Point(28, 69);
+            this.Btn_strng.Name = "Btn_strng";
+            this.Btn_strng.Size = new System.Drawing.Size(155, 38);
+            this.Btn_strng.TabIndex = 2;
+            this.Btn_strng.Text = "String";
+            this.Btn_strng.UseVisualStyleBackColor = false;
+            this.Btn_strng.Click += new System.EventHandler(this.Btn_strng_Click);
+            // 
+            // lblchange
+            // 
+            this.lblchange.AutoSize = true;
+            this.lblchange.Location = new System.Drawing.Point(189, 31);
+            this.lblchange.Name = "lblchange";
+            this.lblchange.Size = new System.Drawing.Size(0, 15);
+            this.lblchange.TabIndex = 3;
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnexit.Location = new System.Drawing.Point(28, 303);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(75, 23);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // lblfirst
+            // 
+            this.lblfirst.AutoSize = true;
+            this.lblfirst.Location = new System.Drawing.Point(28, 130);
+            this.lblfirst.Name = "lblfirst";
+            this.lblfirst.Size = new System.Drawing.Size(59, 15);
+            this.lblfirst.TabIndex = 6;
+            this.lblfirst.Text = "Firstname";
+            // 
+            // lbllast
+            // 
+            this.lbllast.AutoSize = true;
+            this.lbllast.Location = new System.Drawing.Point(28, 176);
+            this.lbllast.Name = "lbllast";
+            this.lbllast.Size = new System.Drawing.Size(58, 15);
+            this.lbllast.TabIndex = 7;
+            this.lbllast.Text = "Lastname";
+            // 
+            // lblage
+            // 
+            this.lblage.AutoSize = true;
+            this.lblage.Location = new System.Drawing.Point(28, 217);
+            this.lblage.Name = "lblage";
+            this.lblage.Size = new System.Drawing.Size(28, 15);
+            this.lblage.TabIndex = 8;
+            this.lblage.Text = "Age";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 214);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // btndisplay
+            // 
+            this.btndisplay.BackColor = System.Drawing.Color.PeachPuff;
+            this.btndisplay.Location = new System.Drawing.Point(28, 263);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(75, 23);
+            this.btndisplay.TabIndex = 12;
+            this.btndisplay.Text = "Display";
+            this.btndisplay.UseVisualStyleBackColor = false;
+            this.btndisplay.Click += new System.EventHandler(this.btndisplay_Click);
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(199, 130);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(0, 15);
+            this.lblname.TabIndex = 13;
+            // 
+            // btnfloat
+            // 
+            this.btnfloat.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnfloat.Location = new System.Drawing.Point(350, 69);
+            this.btnfloat.Name = "btnfloat";
+            this.btnfloat.Size = new System.Drawing.Size(155, 38);
+            this.btnfloat.TabIndex = 14;
+            this.btnfloat.Text = "Float";
+            this.btnfloat.UseVisualStyleBackColor = false;
+            this.btnfloat.Click += new System.EventHandler(this.btnfloat_Click);
+            // 
+            // btnadd
+            // 
+            this.btnadd.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnadd.Location = new System.Drawing.Point(502, 230);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(157, 38);
+            this.btnadd.TabIndex = 15;
+            this.btnadd.Text = "Addition";
+            this.btnadd.UseVisualStyleBackColor = false;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // lbladd
+            // 
+            this.lbladd.AutoSize = true;
+            this.lbladd.Location = new System.Drawing.Point(637, 176);
+            this.lbladd.Name = "lbladd";
+            this.lbladd.Size = new System.Drawing.Size(0, 15);
+            this.lbladd.TabIndex = 16;
+            // 
+            // tbnum1
+            // 
+            this.tbnum1.Location = new System.Drawing.Point(531, 172);
+            this.tbnum1.Name = "tbnum1";
+            this.tbnum1.Size = new System.Drawing.Size(100, 23);
+            this.tbnum1.TabIndex = 17;
+            // 
+            // tbnum2
+            // 
+            this.tbnum2.Location = new System.Drawing.Point(531, 201);
+            this.tbnum2.Name = "tbnum2";
+            this.tbnum2.Size = new System.Drawing.Size(100, 23);
+            this.tbnum2.TabIndex = 18;
+            // 
+            // minusbtn
+            // 
+            this.minusbtn.BackColor = System.Drawing.Color.PeachPuff;
+            this.minusbtn.Location = new System.Drawing.Point(763, 230);
+            this.minusbtn.Name = "minusbtn";
+            this.minusbtn.Size = new System.Drawing.Size(157, 38);
+            this.minusbtn.TabIndex = 19;
+            this.minusbtn.Text = "Subtraction";
+            this.minusbtn.UseVisualStyleBackColor = false;
+            this.minusbtn.Click += new System.EventHandler(this.minusbtn_Click);
+            // 
+            // tbnum3
+            // 
+            this.tbnum3.Location = new System.Drawing.Point(787, 172);
+            this.tbnum3.Name = "tbnum3";
+            this.tbnum3.Size = new System.Drawing.Size(100, 23);
+            this.tbnum3.TabIndex = 20;
+            this.tbnum3.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // tbnum4
+            // 
+            this.tbnum4.Location = new System.Drawing.Point(787, 201);
+            this.tbnum4.Name = "tbnum4";
+            this.tbnum4.Size = new System.Drawing.Size(100, 23);
+            this.tbnum4.TabIndex = 21;
+            // 
+            // lblsub
+            // 
+            this.lblsub.AutoSize = true;
+            this.lblsub.Location = new System.Drawing.Point(893, 175);
+            this.lblsub.Name = "lblsub";
+            this.lblsub.Size = new System.Drawing.Size(0, 15);
+            this.lblsub.TabIndex = 22;
+            // 
+            // tbnum5
+            // 
+            this.tbnum5.Location = new System.Drawing.Point(531, 292);
+            this.tbnum5.Name = "tbnum5";
+            this.tbnum5.Size = new System.Drawing.Size(100, 23);
+            this.tbnum5.TabIndex = 23;
+            // 
+            // tbnum6
+            // 
+            this.tbnum6.Location = new System.Drawing.Point(531, 321);
+            this.tbnum6.Name = "tbnum6";
+            this.tbnum6.Size = new System.Drawing.Size(100, 23);
+            this.tbnum6.TabIndex = 24;
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(502, 350);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(157, 38);
+            this.btndiv.TabIndex = 25;
+            this.btndiv.Text = "Division";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // lbldiv
+            // 
+            this.lbldiv.AutoSize = true;
+            this.lbldiv.Location = new System.Drawing.Point(637, 295);
+            this.lbldiv.Name = "lbldiv";
+            this.lbldiv.Size = new System.Drawing.Size(0, 15);
+            this.lbldiv.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1038, 619);
+            this.Controls.Add(this.lbldiv);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.tbnum6);
+            this.Controls.Add(this.tbnum5);
+            this.Controls.Add(this.lblsub);
+            this.Controls.Add(this.tbnum4);
+            this.Controls.Add(this.tbnum3);
+            this.Controls.Add(this.minusbtn);
+            this.Controls.Add(this.tbnum2);
+            this.Controls.Add(this.tbnum1);
+            this.Controls.Add(this.lbladd);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.btnfloat);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.btndisplay);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblage);
+            this.Controls.Add(this.lbllast);
+            this.Controls.Add(this.lblfirst);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.lblchange);
+            this.Controls.Add(this.Btn_strng);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btn_integer);
+            this.Name = "Form1";
+            this.Text = "My Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_integer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Btn_strng;
+        private System.Windows.Forms.Label lblchange;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label lblfirst;
+        private System.Windows.Forms.Label lbllast;
+        private System.Windows.Forms.Label lblage;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btndisplay;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Button btnfloat;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label lbladd;
+        private System.Windows.Forms.TextBox tbnum1;
+        private System.Windows.Forms.TextBox tbnum2;
+        private System.Windows.Forms.Button minusbtn;
+        private System.Windows.Forms.TextBox tbnum3;
+        private System.Windows.Forms.TextBox tbnum4;
+        private System.Windows.Forms.Label lblsub;
+        private System.Windows.Forms.TextBox tbnum5;
+        private System.Windows.Forms.TextBox tbnum6;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Label lbldiv;
+    }
+}
+
---
+++ That funny thing you know from the other day at the pot luck/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace That_funny_thing_you_know_from_the_other_day_at_the_pot_luck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_integer_Click(object sender, EventArgs e)
+        {
+            int myInteger;
+            myInteger = 25;
+            MessageBox.Show(myInteger.ToString());
+
+        }
+
+        private void Btn_strng_Click(object sender, EventArgs e)
+        {
+            string Writing = textBox1.Text;
+            /*MessageBox.Show(firstName);*/
+            lblchange.Text = Writing;
+        
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btndisplay_Click(object sender, EventArgs e)
+        {
+            string firstName = textBox2.Text;
+            string lastName = textBox3.Text;
+            string years = textBox4.Text;
+            lblname.Text = textBox2.Text + " " + textBox3.Text + " " + textBox4.Text;
+
+        }
+
+        private void btnfloat_Click(object sender, EventArgs e)
+        {
+            float myFloat;
+            myFloat = 0.48F;
+            MessageBox.Show(myFloat.ToString());
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+        
+            int integersum;
+            var num1 = Convert.ToInt32(tbnum1.Text);
+            var num2 = Convert.ToInt32(tbnum2.Text);
+
+            integersum = num1 + num2;
+
+            lbladd.Text = integersum.ToString();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minusbtn_Click(object sender, EventArgs e)
+        {
+            int integersub;
+            var num3 = Convert.ToInt32(tbnum3.Text);
+            var num4 = Convert.ToInt32(tbnum4.Text);
+
+            integersub = num3 - num4;
+
+            lblsub.Text = integersub.ToString();
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            int integerdiv;
+            var num5 = Convert.ToInt32(tbnum5.Text);
+            var num6 = Convert.ToInt32(tbnum6.Text);
+
+            integerdiv = num5 / num6;
+
+            integerdiv = (int)Convert.ToSingle(integerdiv);
+
+            lbldiv.Text = integerdiv.ToString();
+        }
+    }
+}
